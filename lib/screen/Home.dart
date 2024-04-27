31e2c8bcf0280bcd6dd33143fe976256f06265d9
H4DRI165 <hadrizamir2@gmail.com>
2024-04-27 18:06:23 +0800

    Create todo item using List tile

--- lib/screen/Home.dart
+++ lib/screen/Home.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import '../model/todo.dart';
+import '../widgets/todo_item.dart';
 import '../constants/colours.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  final todosList = ToDo.todoList();
 
   @override
   Widget build(BuildContext context) {
@@ -10,28 +14,55 @@ class Home extends StatelessWidget {
       backgroundColor: bgColor,
       appBar: _buildAppBar(),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(0),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.black,
-                    size: 20,
+            searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 50, bottom: 20),
+                    child: Text(
+                      'All Todos',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                    ),
                   ),
-                  prefixIconConstraints: BoxConstraints(
-                    maxHeight: 20,
-                    minWidth: 25,
-                  ),
-                ),
+
+                  for (ToDo todoo in todosList)
+                  ToDoItem(todo: todoo,),
+                ],
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.black,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: grey),
         ),
       ),
     );
